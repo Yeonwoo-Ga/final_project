d18e93fac8f0df1f4a0776e1d899378edb54dece
bbkang <kbb31865@naver.com>
2022-08-22 14:43:39 +0900

    Swagger,Security 설정 추가

--- zproject0822/src/main/java/com/example/demo/Zproject0822Application.java
+++ zproject0822/src/main/java/com/example/demo/Zproject0822Application.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class Zproject0822Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Zproject0822Application.class, args);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
